83d59bbee317d191f3e7a038ee10f2efc8c8fd8d
xmstu <1552421583@qq.com>
2018-03-24 08:24:21 +0800

    08.加入静态资源

--- dailyfresh/apps/users/urls.py
+++ dailyfresh/apps/users/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from apps.users import views
+
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^register$', views.register)
 
 ]
--- dailyfresh/apps/users/views.py
+++ dailyfresh/apps/users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def register(request):
+    """进入注册界面"""
+    return render(request, '')
